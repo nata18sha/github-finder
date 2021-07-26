2a6098ec4c724de49223a5c10f6c866d24a53b86
nata18sha <nata18sha@gmail.com>
2021-07-26 16:18:03 +0300

    logo homepage link added

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -2,16 +2,16 @@ import React, { useContext } from 'react';
 import AlertContext from '../../contex/alert/alertContext';
 
 const Alert = () => {
-  const alertContext = useContext(AlertContext);
-  const { alert } = alertContext;
+	const alertContext = useContext(AlertContext);
+	const { alert } = alertContext;
 
-  return (
-    alert !== null && (
-      <div className={`alert alert-${alert.type}`}>
-        <i className="fas fa-info-circle" /> {alert.message}
-      </div>
-    )
-  );
+	return (
+		alert !== null && (
+			<div className={`alert alert-${alert.type}`}>
+				<i className='fas fa-info-circle' /> {alert.message}
+			</div>
+		)
+	);
 };
 
 export default Alert;
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -3,49 +3,49 @@ import GithubContext from '../../contex/github/githubContex';
 import AlertContext from '../../contex/alert/alertContext';
 
 const Search = () => {
-  const githubContex = useContext(GithubContext);
-  const alertContex = useContext(AlertContext);
+	const githubContex = useContext(GithubContext);
+	const alertContex = useContext(AlertContext);
 
-  const [text, setText] = useState('');
+	const [text, setText] = useState('');
 
-  const onChange = (e) => setText(e.target.value);
+	const onChange = (e) => setText(e.target.value);
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (text === '') {
-      alertContex.setAlert('Please enter something to search', 'light');
-    } else {
-      githubContex.searchUsers(text);
-      setText('');
-    }
-  };
+	const onSubmit = (e) => {
+		e.preventDefault();
+		if (text === '') {
+			alertContex.setAlert('Please enter something to search', 'light');
+		} else {
+			githubContex.searchUsers(text);
+			setText('');
+		}
+	};
 
-  return (
-    <div>
-      <form onSubmit={onSubmit} className="form">
-        <input
-          type="text"
-          name="text"
-          placeholder="Search Users..."
-          value={text}
-          onChange={onChange}
-        />
-        <input
-          type="submit"
-          value="Search"
-          className="btn btn-dark btn-block"
-        />
-      </form>
-      {githubContex.users.length > 0 && (
-        <button
-          className="btn btn-light btn-block"
-          onClick={githubContex.clearUsers}
-        >
-          Clear
-        </button>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			<form onSubmit={onSubmit} className="form">
+				<input
+					type="text"
+					name="text"
+					placeholder="Search Users..."
+					value={text}
+					onChange={onChange}
+				/>
+				<input
+					type="submit"
+					value="Search"
+					className="btn btn-dark btn-block"
+				/>
+			</form>
+			{githubContex.users.length > 0 && (
+				<button
+					className="btn btn-light btn-block"
+					onClick={githubContex.clearUsers}
+				>
+					Clear
+				</button>
+			)}
+		</div>
+	);
 };
 
 export default Search;
